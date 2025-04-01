13693a3e7d1bd29e63b49c99dc0cffd1b987b8d9
gabrielmcafe <gabrielcafe18@gmail.com>
2025-04-01 17:19:14 -0300

    at1

--- Exercícios/A1/Ex1/entregas/Gabriel Café/q1.c
+++ Exercícios/A1/Ex1/entregas/Gabriel Café/q1.c
@@ -7,6 +7,7 @@ void dobra(int *n) {
 int main() {
     int x = 4;
     dobra(&x);
-    printf("%d\n", x);
+    printf("%d\n", x); //nessa questão o ponteiro é aumentado se multiplicando por 2,então quando for 2 será 4,quando for 4 será 8 assim por diante
+    
     return 0;
 }
