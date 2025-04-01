7f3d297b501a927fca88bca6d8b9bc642fcc8dc4
gabrielmcafe <gabrielcafe18@gmail.com>
2025-04-01 17:38:47 -0300

    at1

--- Exercícios/A1/Ex1/entregas/Gabriel Café/q2.c
+++ Exercícios/A1/Ex1/entregas/Gabriel Café/q2.c
@@ -7,7 +7,7 @@ void trocar(int *a, int *b) {
 }
 
 int main() {
-    int x = 10, y = 20;
+    int x = 10, y = 20;//no codigo de passagem corrigimos as variaveis com isso podemos acessar e modificar os valores das variaveis
     trocar(x, y);
     printf("x = %d, y = %d\n", x, y);
     return 0;
